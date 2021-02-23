080375b22efe56d30c81819e1a12faf97a7bcf64
Linquize <linquize@yahoo.com.hk>
2021-02-23 22:20:26 +0800

    Do not compile sqlite with -tags no_sqlite

--- internal/db/db.go
+++ internal/db/db.go
@@ -16,7 +16,6 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mssql"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/pkg/errors"
 	log "unknwon.dev/clog/v2"
 
---
+++ internal/db/sqlite.go
@@ -0,0 +1,5 @@
+// +build !no_sqlite
+
+package db
+
+import _ "github.com/jinzhu/gorm/dialects/sqlite"
